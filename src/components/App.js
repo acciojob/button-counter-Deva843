358f11c5a48ea7063cb53eab8ae16106c23652b4
DevaKumar <137104336+Deva843@users.noreply.github.com>
2023-12-14 12:47:39 +0000

    ButtonCounter

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,41 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [count,setCount] = useState(0);
+  
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {count} times</p>
+        <button onClick={()=>setCount(count + 1)}>click</button>
     </div>
   )
 }
 
 export default App
+
+
+// above and below both codes will do the same thing
+
+
+// import React, { useState } from "react";
+// import './../styles/App.css';
+
+// const App = () => {
+//   let [count,setCount] = useState(0);
+//   function update(){
+//     setCount(count + 1);
+
+//   }
+//   return (
+//     <div>
+//         {/* Do not remove the main div */}
+//         <p>Button clicked {count} times</p>
+//         <button onClick={update}>click</button>
+//     </div>
+//   )
+// }
+
+// export default App
